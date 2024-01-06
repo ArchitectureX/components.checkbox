ad7701276ee8d6386b46d228d1a858c978310138
FoggDev <51180821+FoggDev@users.noreply.github.com>
2024-01-05 23:36:42 -0800

    fix: Adding border

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -10,7 +10,11 @@ const Checkbox: FC<CheckboxProps> = ({ checked, onChange, label }) => {
   return (
     <label className="inline-flex items-center cursor-pointer">
       <input type="checkbox" className="hidden" checked={checked} onChange={onChange} />
-      <span className={`w-6 h-6 inline-block rounded ${checked ? 'bg-blue-600' : 'bg-gray-200'}`}>
+      <span
+        className={`w-6 h-6 inline-block rounded ${
+          checked ? 'bg-blue-600' : 'bg-gray-200 border border-gray-400'
+        }`}
+      >
         {checked && (
           <svg className="fill-current text-white w-6 h-6" viewBox="0 0 20 20">
             <path d="M7.629 14.957l-3.629-3.457 1.8-1.8 1.829 1.743 4.371-4.743 1.8 1.8-6.171 6.457z" />
