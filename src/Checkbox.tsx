759bc456267d68c6d2d9e7fe685492bea446d23a
FoggDev <51180821+FoggDev@users.noreply.github.com>
2024-01-06 00:34:18 -0800

    fix: Adding label wrapper

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -17,29 +17,29 @@ const Checkbox: FC<Props> = ({ checked, onChange, label, helpText, disabled = fa
 
   return (
     <div className="flex" style={{ margin: '0 auto', width: '93%' }}>
-      <div className="flex items-center h-5">
+      <label className="flex items-center cursor-pointer">
         <input
           type="checkbox"
           value=""
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 cursor-pointer"
           checked={checked}
           onChange={onChange}
-        />{' '}
-      </div>
-      <div className="ms-2 text-sm" style={{ marginLeft: '10px' }}>
-        <div
-          className={cx.join(
-            'ms-2 text-sm font-medium text-left',
-            disabled ? classes.disabled : classes.label
-          )}
-        >
-          {label}
-        </div>
+        />
+        <div className="ms-2 text-sm" style={{ marginLeft: '10px' }}>
+          <div
+            className={cx.join(
+              'ms-2 text-sm font-medium text-left',
+              disabled ? classes.disabled : classes.label
+            )}
+          >
+            {label}
+          </div>
 
-        {helpText && (
-          <div className="text-xs font-normal text-gray-500 dark:text-gray-300">{helpText}</div>
-        )}
-      </div>
+          {helpText && (
+            <div className="text-xs font-normal text-gray-500 dark:text-gray-300">{helpText}</div>
+          )}
+        </div>
+      </label>
     </div>
   )
 }
