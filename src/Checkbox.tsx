e6d2b07635e233609d878cb56b286d9e4fd502ca
FoggDev <51180821+FoggDev@users.noreply.github.com>
2024-01-06 00:08:34 -0800

    fix: Fixing styles

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -16,23 +16,29 @@ const Checkbox: FC<Props> = ({ checked, onChange, label, helpText, disabled = fa
   }
 
   return (
-    <div className="flex items-center mb-4">
-      <input
-        type="checkbox"
-        value=""
-        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        checked={checked}
-        onChange={onChange}
-      />
-      <label
-        className={cx.join('ms-2 text-sm font-medium', disabled ? classes.disabled : classes.label)}
-      >
-        {label}
-      </label>
-
-      {helpText && (
-        <p className="text-xs font-normal text-gray-500 dark:text-gray-300">{helpText}</p>
-      )}
+    <div className="flex">
+      <div className="flex items-center h-5">
+        <input
+          type="checkbox"
+          value=""
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          checked={checked}
+          onChange={onChange}
+        />{' '}
+      </div>
+      <div className="ms-2 text-sm">
+        <label
+          className={cx.join(
+            'ms-2 text-sm font-medium',
+            disabled ? classes.disabled : classes.label
+          )}
+        >
+          {label}
+        </label>
+        {helpText && (
+          <p className="text-xs font-normal text-gray-500 dark:text-gray-300">{helpText}</p>
+        )}
+      </div>
     </div>
   )
 }
